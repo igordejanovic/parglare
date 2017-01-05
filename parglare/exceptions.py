5bc767280fdc872bb213e2a9dba10ee362ed3cd7
Igor Dejanovic <igor.dejanovic@gmail.com>
2017-01-05 14:11:07 +0100

    Keep position and list of symbols in ParseError.

--- parglare/exceptions.py
+++ parglare/exceptions.py
@@ -6,6 +6,8 @@ class NotInitialized(Exception):
 
 class ParseError(Exception):
     def __init__(self, position, symbols):
+        self.position = position
+        self.symbols = symbols
         super(ParseError, self).__init__(
             "Error at position %d. Expected: %s" %
             (position, " or ".join([s.name for s in symbols])))
