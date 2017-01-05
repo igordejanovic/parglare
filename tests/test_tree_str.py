29d235b0527266f9db88018974892cde4da5d53b
Igor Dejanovic <igor.dejanovic@gmail.com>
2017-01-05 21:15:10 +0100

    Test parse tree printing.

---
+++ tests/test_tree_str.py
@@ -0,0 +1,18 @@
+import pytest
+from parglare.parser import Parser
+from .expression_grammar import get_grammar, E
+
+
+def test_tree_str():
+
+    grammar = get_grammar()
+    p = Parser(grammar, E)
+
+    res = p.parse("""id+  id * (id
+    +id  )
+    """)
+
+    ts = res.tree_str()
+
+    assert '+[18, +]' in ts
+    assert ')[23, )]' in ts
