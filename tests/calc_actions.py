c273b9f3137be6dc2bbdd0873ddae98ded781710
Igor Dejanovic <igor.dejanovic@gmail.com>
2017-01-12 11:36:30 +0100

    Refactoring calc test. Move actions to a separate module.

---
+++ tests/calc_actions.py
@@ -0,0 +1,24 @@
+def act_assignment(context, nodes):
+    name = nodes[0]
+    number = nodes[2]
+
+    if not hasattr(context, 'variables'):
+        context.variables = {}
+
+    context.variables[name] = number
+
+
+actions = {
+    "Calc": lambda _, nodes: nodes[1],
+    "Assignment": act_assignment,
+    "E:0": lambda _, nodes: nodes[0] + nodes[2],
+    "E:1": lambda _, nodes: nodes[0] - nodes[2],
+    "E:2": lambda _, nodes: nodes[0] * nodes[2],
+    "E:3": lambda _, nodes: nodes[0] / nodes[2],
+    "E:4": lambda _, nodes: nodes[1],
+    "E:5": lambda _, nodes: nodes[0],
+    "E:6": lambda _, nodes: nodes[0],
+    "Number": lambda _, value: float(value),
+    "VariableName": lambda _, value: value,
+    "VariableRef": lambda context, nodes: context.variables[nodes[0]],
+}
--- tests/test_load_from_file.py
+++ tests/test_load_from_file.py
@@ -1,33 +1,10 @@
 import pytest
 import os
 from parglare import Grammar, Parser
+from .calc_actions import actions
 
 
 def test_load_from_file():
-
-    def act_assignment(context, nodes):
-        name = nodes[0]
-        number = nodes[2]
-
-        if not hasattr(context, 'variables'):
-            context.variables = {}
-
-        context.variables[name] = number
-
-    actions = {
-        "Calc": lambda _, nodes: nodes[1],
-        "Assignment": act_assignment,
-        "E:0": lambda _, nodes: nodes[0] + nodes[2],
-        "E:1": lambda _, nodes: nodes[0] - nodes[2],
-        "E:2": lambda _, nodes: nodes[0] * nodes[2],
-        "E:3": lambda _, nodes: nodes[0] / nodes[2],
-        "E:4": lambda _, nodes: nodes[1],
-        "E:5": lambda _, nodes: nodes[0],
-        "E:6": lambda _, nodes: nodes[0],
-        "Number": lambda _, value: float(value),
-        "VariableName": lambda _, value: value,
-        "VariableRef": lambda context, nodes: context.variables[nodes[0]],
-    }
 
     grammar = Grammar.from_file(os.path.join(
         os.path.dirname(__file__), 'calc.pg'))
