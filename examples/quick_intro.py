9cb2d1c9ed4eb6988d72951290bdefdea2448914
Igor Dejanovic <igor.dejanovic@gmail.com>
2017-05-23 19:27:23 +0200

    Quick intro example.

---
+++ examples/quick_intro.py
@@ -0,0 +1,36 @@
+from parglare import Parser, Grammar
+
+grammar = r"""
+E = E '+' E  {left, 1}
+  | E '-' E  {left, 1}
+  | E '*' E  {left, 2}
+  | E '/' E  {left, 2}
+  | E '^' E  {right, 3}
+  | '(' E ')'
+  | number;
+number = /\d+(\.\d+)?/;
+"""
+
+actions = {
+    "E": [lambda _, nodes: nodes[0] + nodes[2],
+          lambda _, nodes: nodes[0] - nodes[2],
+          lambda _, nodes: nodes[0] * nodes[2],
+          lambda _, nodes: nodes[0] / nodes[2],
+          lambda _, nodes: nodes[0] ** nodes[2],
+          lambda _, nodes: nodes[1],
+          lambda _, nodes: nodes[0]],
+    "number": lambda _, value: float(value),
+}
+
+g = Grammar.from_string(grammar)
+parser = Parser(g, debug=True, actions=actions)
+
+result = parser.parse("34 + 4.6 / 2 * 4^2^2 + 78")
+
+print("Result = ", result)
+
+# Output
+# -- Debuging/tracing output with detailed info about grammar, productions,
+# -- terminals and nonterminals, DFA states, parsing progress,
+# -- and at the end of the output:
+# Result = 700.8
