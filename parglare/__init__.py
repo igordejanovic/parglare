1b3f767ceedd7846ed94659e3fbf9175653f28cc
Igor Dejanovic <igor.dejanovic@gmail.com>
2017-07-19 12:04:27 +0200

    Export pos_to_line_col and Error in root package.

--- parglare/__init__.py
+++ parglare/__init__.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 # flake8: NOQA
-from parglare.parser import Parser, LALR, SLR
+from parglare.parser import Parser, LALR, SLR, pos_to_line_col
 from parglare.glr import GLRParser
 from parglare.grammar import Grammar, NonTerminal, Terminal, \
     RegExRecognizer, EMPTY, EOF
+from parglare.errors import Error
 from parglare.exceptions import ParseError, GrammarError
 
 __author__ = """Igor R. Dejanovic"""
