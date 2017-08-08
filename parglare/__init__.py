e51949da7d95e84283c26d4ea5178a873f892828
Igor Dejanovic <igor.dejanovic@gmail.com>
2017-08-08 13:38:48 +0200

    Exported `Token` in parglare package.

--- parglare/__init__.py
+++ parglare/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # flake8: NOQA
-from parglare.parser import Parser, LALR, SLR, pos_to_line_col
+from parglare.parser import Parser, Token, LALR, SLR, pos_to_line_col
 from parglare.glr import GLRParser
 from parglare.grammar import Grammar, NonTerminal, Terminal, \
     RegExRecognizer, EMPTY, EOF
