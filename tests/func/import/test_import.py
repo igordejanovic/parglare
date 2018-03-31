34c6923e7248053eb9ec48170da794cb77498b23
Igor Dejanovic <igor.dejanovic@gmail.com>
2018-03-31 17:51:01 +0200

    WIP Start of tests for import.

---
+++ tests/func/import/test_import.py
@@ -0,0 +1,9 @@
+import os
+from parglare import Grammar
+
+this_folder = os.path.dirname(__file__)
+
+
+def test_import():
+    g = Grammar.from_file(os.path.join(this_folder, 'frist.pg'))
+    assert g
