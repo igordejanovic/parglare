3d10070b97159183708c6d78b4df1f2848623986
Igor Dejanovic <igor.dejanovic@gmail.com>
2018-04-05 12:29:19 +0200

    Fixing import tests.

--- tests/func/import/test_import.py
+++ tests/func/import/test_import.py
@@ -5,5 +5,5 @@ this_folder = os.path.dirname(__file__)
 
 
 def test_import():
-    g = Grammar.from_file(os.path.join(this_folder, 'frist.pg'))
+    g = Grammar.from_file(os.path.join(this_folder, 'first.pg'))
     assert g
