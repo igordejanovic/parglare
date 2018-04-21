6ddce847390688ad2b37c44ad362c2e168a01608
Igor Dejanovic <igor.dejanovic@gmail.com>
2018-04-21 10:50:47 +0200

    Diamond import test.

---
+++ tests/func/import/diamond/test_diamond.py
@@ -0,0 +1,29 @@
+import os
+from parglare import Grammar, Parser
+
+this_folder = os.path.dirname(__file__)
+
+
+def test_diamond_import():
+    g = Grammar.from_file(os.path.join(this_folder, 'model.pg'))
+    assert g
+
+    input_str = '''
+
+    package First
+    package Second
+
+    module SomeModule {
+
+        component myComponent {
+            in SomeInputSlot
+            out SomeOutputSlot
+        }
+
+
+    }
+
+    '''
+
+    result = Parser(g).parse(input_str)
+    assert result
