d395adadbaaac6c6e680c1d30150e54776f1fef4
Igor Dejanovic <igor.dejanovic@gmail.com>
2018-05-22 13:29:19 +0200

    fix #32 Fixed conflict between string match and rule with the same name

---
+++ tests/func/regressions/test_issue32.py
@@ -0,0 +1,12 @@
+import pytest
+from parglare import Grammar, GrammarError
+
+
+def test_conflict_between_string_match_and_rule_with_same_name():
+
+    with pytest.raises(GrammarError, matches='*already defined*'):
+        Grammar.from_string('''
+            Terminals:
+                "Terminals" ":" terminal_list=Terminal* ";"
+            ;
+        ''')
