e3979f500e675a1c61a93d1c8c4e2eac8265d58e
Jeff Webb <jeff.webb@codecraftsmen.org>
2018-08-03 22:29:06 -0500

    Added test_recognizer_context to increase testsuite coverage.

---
+++ tests/func/test_recognizer_context.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+import pytest  # noqa
+from parglare import Grammar, Parser
+import re
+
+def test_recognizer_context():
+    grammar = """
+    program: expression+[semicolon];
+    expression: term+;
+
+    terminals
+    semicolon: ";";
+    term:;
+    """
+
+    term_re = re.compile(r"[a-zA-Z_]+")
+    def term(context, input, pos):
+        match = term_re.match(input, pos)
+        if match is None:
+            return None
+        return input[pos:match.end()]
+
+    g = Grammar.from_string(grammar, recognizers={'term': term})
+    parser = Parser(g)
+    assert parser.parse("a bb cc; d ee f; g hh i")
