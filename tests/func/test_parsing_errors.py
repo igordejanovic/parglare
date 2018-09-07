6e9220b387d8c09a3d229678c6f8fae0daa91500
Igor Dejanovic <igor.dejanovic@gmail.com>
2018-09-07 18:28:42 +0200

    Updating tests for error handling.

--- tests/func/test_parsing_errors.py
+++ tests/func/test_parsing_errors.py
@@ -2,15 +2,18 @@
 from __future__ import unicode_literals
 import pytest  # noqa
 import os
-from parglare.parser import Parser
-from parglare.exceptions import ParseError
+from parglare import Grammar, Parser, GLRParser, ParseError
 from .expression_grammar import get_grammar
 
 
-def test_invalid_input():
+parsers = pytest.mark.parametrize("parser_class", [Parser, GLRParser])
+
+
+@parsers
+def test_invalid_input(parser_class):
 
     grammar = get_grammar()
-    p = Parser(grammar)
+    p = parser_class(grammar)
 
     with pytest.raises(ParseError) as e:
         p.parse("id+id*+id")
@@ -18,75 +21,104 @@ def test_invalid_input():
     assert e.value.location.start_position == 6
     assert "(" in str(e)
     assert "id" in str(e)
+    assert '*' in [s.name for s in e.value.symbols_before]
+    assert '+' in [t.value for t in e.value.tokens_ahead]
+    expected_names = [s.name for s in e.value.symbols_expected]
+    assert 'id' in expected_names
+    assert '(' in expected_names
 
 
-def test_premature_end():
+@parsers
+def test_premature_end(parser_class):
 
     grammar = get_grammar()
-    p = Parser(grammar)
+    p = parser_class(grammar)
 
     with pytest.raises(ParseError) as e:
         p.parse("id+id*")
 
     assert e.value.location.start_position == 6
-    assert "(" in str(e)
-    assert "id" in str(e)
+    expected_names = [s.name for s in e.value.symbols_expected]
+    assert 'id' in expected_names
+    assert '(' in expected_names
+    assert '*' in [s.name for s in e.value.symbols_before]
+    assert e.value.tokens_ahead == []
 
 
-def test_line_column():
+def test_ambiguous_glr():
+    grammar = r"""
+    S: E EOF;
+    E: E '+' E
+     | E '*' E
+     | number;
+
+    terminals
+    number: /\d+(\.\d+)?/;
+    """
+    g = Grammar.from_string(grammar)
+    parser = GLRParser(g)
+
+    with pytest.raises(ParseError) as e:
+        parser.parse("1 + 2 * 3 / 5")
+
+    assert e.value.location.start_position == 10
+    assert 'number' in [s.name for s in e.value.symbols_before]
+
+
+@parsers
+def test_line_column(parser_class):
     grammar = get_grammar()
-    p = Parser(grammar)
+    p = parser_class(grammar)
 
-    try:
+    with pytest.raises(ParseError) as e:
         p.parse("""id + id * id + id + error * id""")
-    except ParseError as e:
-        loc = e.location
-        assert loc.start_position == 20
-        assert loc.line == 1
-        assert loc.column == 20
 
-    try:
+    loc = e.value.location
+    assert loc.start_position == 20
+    assert loc.line == 1
+    assert loc.column == 20
+
+    with pytest.raises(ParseError) as e:
         p.parse("""id + id * id + id + error * id
 
         """)
-    except ParseError as e:
-        loc = e.location
-        assert loc.start_position == 20
-        assert loc.line == 1
-        assert loc.column == 20
+    loc = e.value.location
+    assert loc.start_position == 20
+    assert loc.line == 1
+    assert loc.column == 20
 
-    try:
+    with pytest.raises(ParseError) as e:
         p.parse("""
 
 id + id * id + id + error * id""")
-    except ParseError as e:
-        loc = e.location
-        assert loc.start_position == 22
-        assert loc.line == 3
-        assert loc.column == 20
+    loc = e.value.location
+    assert loc.start_position == 22
+    assert loc.line == 3
+    assert loc.column == 20
 
-    try:
+    with pytest.raises(ParseError) as e:
         p.parse("""
 
 id + id * id + id + error * id
 
         """)
-    except ParseError as e:
-        loc = e.location
-        assert loc.start_position == 22
-        assert loc.line == 3
-        assert loc.column == 20
+    loc = e.value.location
+    assert loc.start_position == 22
+    assert loc.line == 3
+    assert loc.column == 20
 
 
-def test_file_name():
+@parsers
+def test_file_name(parser_class):
     "Test that file name is given in the error string when parsing file."
     grammar = get_grammar()
-    p = Parser(grammar)
+    p = parser_class(grammar)
 
     input_file = os.path.join(os.path.dirname(__file__),
                               'parsing_errors.txt')
 
-    try:
+    with pytest.raises(ParseError) as e:
         p.parse_file(input_file)
-    except ParseError as e:
-        assert 'parsing_errors.txt' in str(e)
+
+    assert 'parsing_errors.txt' in str(e.value)
+    assert 'parsing_errors.txt' in e.value.location.file_name
