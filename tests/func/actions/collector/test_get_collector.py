fff1d665e1696e4b3c89133c56e338920054b17d
Igor Dejanovic <igor.dejanovic@gmail.com>
2019-04-29 19:44:57 +0200

    Add more test for actions and recognizers and `get_collector` call

---
+++ tests/func/actions/collector/test_get_collector.py
@@ -0,0 +1,67 @@
+from __future__ import unicode_literals
+import pytest
+import os
+import re
+from parglare import get_collector, Parser, Grammar, GrammarError
+
+THIS_FOLDER = os.path.abspath(os.path.dirname(__file__))
+
+
+def test_action_explicit_get_collector():
+    """
+    Test the basic usage of `get_collector` API where we don't provide
+    actions in a separate python module.
+    """
+
+    action = get_collector()
+
+    @action
+    def INT(context, value):
+        return int(value)
+
+    @action
+    def STRING(context, value):
+        return "#{}#".format(value)
+
+    grammar = Grammar.from_file(os.path.join(THIS_FOLDER, 'grammar.pg'))
+    Parser(grammar, actions=action.all)
+
+
+def test_action_explicit_get_collector_missing_action():
+    """
+    Test when `get_collector` has a terminal without defined action nothing
+    happens as the default implicit action will be used.
+    """
+
+    action = get_collector()
+
+    @action
+    def INT(context, value):
+        return int(value)
+
+    grammar = Grammar.from_file(os.path.join(THIS_FOLDER, 'grammar.pg'))
+    Parser(grammar, actions=action.all)
+
+
+def test_actions_explicit_get_collector_action_for_unexisting_terminal():
+    """
+    Test for situation when `get_collector` has an action for un-existing
+    terminal.
+    """
+
+    action = get_collector()
+
+    @action
+    def INT(context, value):
+        return int(value)
+
+    @action
+    def STRING(context, value):
+        return "#{}#".format(value)
+
+    @action
+    def STRING2(context, value):
+        return "#{}#".format(value)
+
+    grammar = Grammar.from_file(os.path.join(THIS_FOLDER, 'grammar.pg'))
+    Parser(grammar, actions=action.all)
---
+++ tests/func/recognizers/collector/test_get_collector.py
@@ -0,0 +1,73 @@
+from __future__ import unicode_literals
+import pytest
+import os
+import re
+from parglare import get_collector, Parser, Grammar, GrammarError
+
+THIS_FOLDER = os.path.abspath(os.path.dirname(__file__))
+
+
+def test_recognizer_explicit_get_collector():
+    """
+    Test the basic usage of `get_collector` API where we don't provide
+    recognizers in a separate python module.
+    """
+
+    recognizer = get_collector()
+
+    @recognizer
+    def INT(input, pos):
+        return re.compile(r'\d+').match(input[pos:])
+
+    @recognizer
+    def STRING(input, pos):
+        return re.compile(r'\d+').match(input[pos:])
+
+    grammar = Grammar.from_file(os.path.join(THIS_FOLDER, 'grammar.pg'),
+                                recognizers=recognizer.all)
+    parser = Parser(grammar)
+    assert parser
+
+
+def test_recognizer_explicit_get_collector_missing_recognizer():
+    """
+    Test when `get_collector` has a terminal without defined recognizer an
+    exception is raised.
+    """
+
+    recognizer = get_collector()
+
+    @recognizer
+    def INT(input, pos):
+        return re.compile(r'\d+').match(input[pos:])
+
+    with pytest.raises(GrammarError,
+                       match=r'Terminal "STRING" has no recognizer defined.'):
+        Grammar.from_file(os.path.join(THIS_FOLDER, 'grammar.pg'),
+                          recognizers=recognizer.all)
+
+
+def test_recognizer_explicit_get_collector_recognizer_for_unexisting_terminal():  # noqa
+    """
+    Test for situation when `get_collector` has a recognizer for un-existing
+    terminal.
+    """
+
+    recognizer = get_collector()
+
+    @recognizer
+    def INT(input, pos):
+        return re.compile(r'\d+').match(input[pos:])
+
+    @recognizer
+    def STRING(input, pos):
+        return re.compile(r'\d+').match(input[pos:])
+
+    @recognizer
+    def STRING2(input, pos):
+        return re.compile(r'\d+').match(input[pos:])
+
+    grammar = Grammar.from_file(os.path.join(THIS_FOLDER, 'grammar.pg'),
+                                recognizers=recognizer.all)
+    parser = Parser(grammar)
+    assert parser
