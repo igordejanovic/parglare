e79b56e67adc0f5ed4c52f59ee2a62f20dae2bc1
Igor Dejanovic <igor.dejanovic@gmail.com>
2019-07-27 20:47:29 +0200

    Fix quick intro example

--- examples/quick_intro.py
+++ examples/quick_intro.py
@@ -1,31 +1,38 @@
-from parglare import Parser, Grammar
+from parglare import Parser, Grammar, Actions
+from operator import add, sub, mul, truediv, pow
 
 grammar = r"""
-E: E '+' E  {left, 1}
- | E '-' E  {left, 1}
- | E '*' E  {left, 2}
- | E '/' E  {left, 2}
- | E '^' E  {right, 3}
- | '(' E ')'
- | number;
+@pass_single Exp: E EOF;
+@op E: E '+' E  {left, 1}
+     | E '-' E  {left, 1}
+     | E '*' E  {left, 2}
+     | E '/' E  {left, 2}
+     | E '^' E  {right, 3}
+     | '(' E ')' {@pass_inner}
+     | number {@pass_single};
 
 terminals
 number: /\d+(\.\d+)?/;
 """
 
-actions = {
-    "E": [lambda _, nodes: nodes[0] + nodes[2],
-          lambda _, nodes: nodes[0] - nodes[2],
-          lambda _, nodes: nodes[0] * nodes[2],
-          lambda _, nodes: nodes[0] / nodes[2],
-          lambda _, nodes: nodes[0] ** nodes[2],
-          lambda _, nodes: nodes[1],
-          lambda _, nodes: nodes[0]],
-    "number": lambda _, value: float(value),
-}
+
+class MyActions(Actions):
+    def op(self, n):
+        opfunc = {
+            '+': add,
+            '-': sub,
+            '*': mul,
+            '/': truediv,
+            '^': pow
+        }[n[1]]
+        return opfunc(n[0], n[2])
+
+    def number(self, n):
+        return float(n)
+
 
 g = Grammar.from_string(grammar)
-parser = Parser(g, debug=True, actions=actions)
+parser = Parser(g, debug=True, actions=MyActions())
 
 result = parser.parse("34 + 4.6 / 2 * 4^2^2 + 78")
 
