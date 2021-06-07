7d9982ab7fea535b0fe29400d19fe76d9b8bcecb
Igor Dejanovic <igor.dejanovic@gmail.com>
2021-06-07 18:02:10 +0200

    Add java example

---
+++ examples/java/README.md
@@ -0,0 +1,9 @@
+This is an implementation of Java SE 16 Edition based on the spec defined in
+https://docs.oracle.com/javase/specs/jls/se16/html/jls-19.html
+
+Since the grammar is derived from the official specification, please see the
+legal terms from the specification if you plan to use it in your project.
+https://docs.oracle.com/javase/specs/jls/se16/html/index.html
+
+The test input file is from the [Spring Boot
+project](https://github.com/spring-projects/spring-boot).
---
+++ examples/java/java_example.py
@@ -0,0 +1,27 @@
+import os
+import time
+from parglare import Grammar, GLRParser
+
+
+def main(debug=False):
+    this_folder = os.path.dirname(__file__)
+
+    g = Grammar.from_file(os.path.join(this_folder, 'java16.pg'))
+    parser = GLRParser(g, debug=debug, debug_colors=True)
+
+    file_name = os.path.join(this_folder, f'TomcatServletWebServerFactory.java')
+    file_size = os.path.getsize(file_name)
+
+    t_start = time.time()
+    forest = parser.parse_file(file_name)
+    t_end = time.time()
+
+    print('Elapsed time: {:.2f}'.format(t_end - t_start), 'sec')
+    print('Speed = {:.2f}'.format(file_size/1000/(t_end - t_start)),
+            'KB/sec\n')
+    print('Solutions: ', forest.solutions)
+    print('Ambiguities: ', forest.ambiguities)
+ 
+
+if __name__ == "__main__":
+    main(debug=False)
