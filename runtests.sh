41fa6f0c48c54cd1ed6077f2d6fd5681f44c455e
Igor Dejanovic <igor.dejanovic@gmail.com>
2023-02-24 10:59:45 +0100

    Fix runtest script, remove six refs

--- runtests.sh
+++ runtests.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Run all tests and generate coverage report
 
-coverage run --omit parglare/six.py,parglare/cli.py --source parglare -m py.test tests/func || exit 1
+coverage run --omit parglare/cli.py --source parglare -m pytest tests/func || exit 1
 coverage report --fail-under 90 || exit 1
 # Run this to generate html report
 # coverage html --directory=coverage
