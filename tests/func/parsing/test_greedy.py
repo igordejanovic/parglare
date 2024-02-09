839a80996ddc40d2bb19e4e4091e7aeeb20a0f14
Igor Dejanovic <igor.dejanovic@gmail.com>
2024-02-09 17:10:50 +0100

    test: more greedy tests

--- tests/func/parsing/test_greedy.py
+++ tests/func/parsing/test_greedy.py
@@ -1,4 +1,5 @@
-from parglare import GLRParser, Grammar
+import pytest
+from parglare import GLRParser, Grammar, ParseError
 
 
 def test_greedy_zero_or_more():
@@ -105,4 +106,86 @@ def test_greedy_optional():
     g = Grammar.from_string(grammar)
     p = GLRParser(g)
     forest = p.parse("a a a a a a")
+    assert len(forest) == 1
+
+
+def test_greedy_interleaved_zero():
+    """
+    Multiple zero or more with optional in between.
+    """
+    grammar = r"""
+    S: A* B? A*;
+    terminals
+    A: "a";
+    B: "b";
+    """
+
+    g = Grammar.from_string(grammar)
+    p = GLRParser(g)
+    assert len(p.table.sr_conflicts) == 2
+    assert len(p.table.rr_conflicts) == 0
+    forest = p.parse("a a a a")
+    assert len(forest) == 5
+    forest = p.parse("a a b a a")
+    assert len(forest) == 1
+    forest = p.parse("a a a b")
+    assert len(forest) == 1
+
+    grammar = r"""
+    S: A*! B? A*;
+    terminals
+    A: "a";
+    B: "b";
+    """
+
+    g = Grammar.from_string(grammar)
+    p = GLRParser(g)
+    assert len(p.table.sr_conflicts) == 0
+    assert len(p.table.rr_conflicts) == 0
+    forest = p.parse("a a a a")
+    assert len(forest) == 1
+    forest = p.parse("a a b a a")
+    assert len(forest) == 1
+
+
+def test_greedy_interleaved_one():
+    """
+    Multiple one or more with optional in between.
+    """
+    grammar = r"""
+    S: A+ B? A+;
+    terminals
+    A: "a";
+    B: "b";
+    """
+
+    g = Grammar.from_string(grammar)
+    p = GLRParser(g)
+    assert len(p.table.sr_conflicts) == 1
+    assert len(p.table.rr_conflicts) == 0
+    forest = p.parse("a a a a")
+    assert len(forest) == 3
+    forest = p.parse("a a b a a")
+    assert len(forest) == 1
+    forest = p.parse("a a a b a")
+    assert len(forest) == 1
+
+    grammar = r"""
+    S: A+! B? A+;
+    terminals
+    A: "a";
+    B: "b";
+    """
+
+    g = Grammar.from_string(grammar)
+    p = GLRParser(g)
+    assert len(p.table.sr_conflicts) == 0
+    assert len(p.table.rr_conflicts) == 0
+
+    # This can't be parsed as A+! will collect all `a` and there will be none
+    # left for the A+ at the end.
+    with pytest.raises(ParseError):
+        forest = p.parse("a a a a")
+
+    forest = p.parse("a a b a a")
     assert len(forest) == 1
