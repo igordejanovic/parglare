db1a75811e189b8d73f0a069e568aea1033e8a4a
Igor Dejanovic <igor.dejanovic@gmail.com>
2026-02-12 21:36:50 +0100

    test: test if parser is deallocated on delete

--- tests/func/regressions/memory_leak/test_memory_leak.py
+++ tests/func/regressions/memory_leak/test_memory_leak.py
@@ -38,3 +38,21 @@ def test_parser_memory_leak_through_storing_exception_instance(parser_type):
 
     # import objgraph
     # objgraph.show_backrefs(dummy_ref(), 10, filename="parglare_parser_backrefs.png")
+
+
+@pytest.mark.parametrize("parser_type", [Parser, GLRParser])
+def test_parser_memory_leak(parser_type):
+    this_folder = dirname(__file__)
+
+    grammar = Grammar.from_file(join(this_folder, "first.pg"))
+    parser = parser_type(grammar)
+    parser_ref = weakref.ref(parser)
+
+    # parser instance should be deallocated on delete as long as we are not
+    # holding onto parse result
+    parser.parse("e e")
+    del parser
+
+    gc.collect()
+
+    assert parser_ref() is None, "Parser is not deallocated"
